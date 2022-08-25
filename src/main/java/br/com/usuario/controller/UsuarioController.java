60a1b431190bac5902901eede11b6f9735852df8
thbd <thiagobalbino91@gmail.com>
2022-08-25 20:36:10 -0300

    add controller user

--- src/main/java/br/com/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/usuario/controller/UsuarioController.java
@@ -1,5 +1,16 @@
 package br.com.usuario.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class UsuarioController {
+	
+	@GetMapping()
+	public String teste() {
+		return "teste ok";
+	}
 
 }
